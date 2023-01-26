f895e2852e404bcc77d0e00e3de211b253360f28
Eugene <jizaazaza@mail.ru>
2023-01-26 15:03:25 +0300

    lesson4.1
    1

---
+++ GameWeb/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    void LateUpdate()
+    {
+        transform.position = _target.position;
+    }
+}
